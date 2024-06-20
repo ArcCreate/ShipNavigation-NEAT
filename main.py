d8ad949aee28a1e3c208c053f7212336a7010ae8
ArcCreate <arckumar12@gmail.com>
2024-06-19 19:06:29 -0700

    Adding on screen statistics

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import HFONT
 import neat
 import os
 import sys
@@ -6,19 +7,26 @@ import pygame
 import math
 from ship import Ship
 
+pygame.init()
+
 #Screen Variables
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 900
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 TRACK = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "Track4.png")), (SCREEN_WIDTH, SCREEN_HEIGHT))
+FONT = pygame.font.Font('freesansbold.ttf', 25)
 
 
 def eval_genomes(genomes, config):
     #global variables for the AI to use
-    global ships, ge, nets
+    global ships, ge, nets, SCORE, best, lastBest
     ships = []
     ge = []
     nets = []
+    SCORE = 0
+    best = 0
+    lastBest = 0
+    clock = pygame.time.Clock()
 
     #connecting all lists
     for genome_id, genome in genomes:
@@ -28,6 +36,20 @@ def eval_genomes(genomes, config):
         net = neat.nn.FeedForwardNetwork.create(genome, config)
         nets.append(net)
         genome.fitness = 0
+    
+    #Screen stats
+    def statistics():
+        global ships, ge, SCORE
+        SCORE += 1
+        text = FONT.render("Current Score: " + str(SCORE), True, (0,0,0))
+        text_1 = FONT.render(f'Ships Alive:  {str(len(ships))}', True, (18, 105, 29))
+        text_2 = FONT.render(f'Current Generation:  {pop.generation+1}', True, (0, 0, 0))
+        text_3 = FONT.render(f'Max Score from ancestor:  {lastBest}', True, (222, 155, 64))
+
+        SCREEN.blit(text, (50, 690))
+        SCREEN.blit(text_1, (50, 730))
+        SCREEN.blit(text_2, (50, 770)) 
+        SCREEN.blit(text_3, (50, 810))
 
     #game loop
     run = True
@@ -39,15 +61,19 @@ def eval_genomes(genomes, config):
 
         #track image on screen
         SCREEN.blit(TRACK, (0, 0))    
+        statistics()
 
         #if no more ships then break out
         if len(ships) == 0:
+            lastBest = best
             break
 
         #collision logic
         for i, ship in enumerate(ships):
-            ge[i].fitness += 0.1
+            ge[i].fitness += 1
             if ship.sprite.play == False:
+                if ge[i].fitness > best:
+                    best = ge[i].fitness
                 removeShip(i)
         
         #inputs and outputs into NEAT
@@ -67,6 +93,7 @@ def eval_genomes(genomes, config):
                 ship.sprite.direction = 0
             
 
+        clock.tick(60)
         # Screen drawing
         for ship in ships:
             ship.draw(SCREEN)
